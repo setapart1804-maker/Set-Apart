f9bf102eb4d728d69d1de7d4b1bb1b1136583b86
SET APART <setapart1804@gmail.com>
2026-09-11 13:21:35 -0400

    Fix price formatting for hoodie items

--- script.js
+++ script.js
@@ -502,7 +502,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 "PILGRIM HOODIE",
 
             price:
-               $34.99,
+               34.99,
 
             images: {
 
@@ -531,7 +531,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 "GOD FIRST HOODIE",
 
             price:
-                $34.99,
+                34.99,
 
             images: {
 
